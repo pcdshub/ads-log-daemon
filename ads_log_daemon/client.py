b377ba1f46a56b57ab688642411a9ac08a3394e1
Ken Lauer <klauer@slac.stanford.edu>
2021-03-04 12:33:59 -0800

    MNT: more resilient connection handling

--- ads_log_daemon/client.py
+++ ads_log_daemon/client.py
@@ -1,9 +1,21 @@
 import asyncio
 import json
+import os
+import sys
+import logging
+from typing import Optional
 
 import ads_async
-from ads_async import structs
+from ads_async import constants, structs
+from ads_async.exceptions import RequestFailedError
 from ads_async.asyncio.client import Client
+from ads_async.bin.info import get_plc_info
+from ads_async.bin.route import add_route_to_plc
+
+LOG_DAEMON_HOST = os.environ.get("LOG_DAEMON_NET_ID", "172.21.32.90")
+LOG_DAEMON_NET_ID = os.environ.get("LOG_DAEMON_NET_ID", f"{LOG_DAEMON_HOST}.1.1")
+LOG_DAEMON_ROUTE_NAME = os.environ.get("LOG_DAEMON_ROUTE_NAME", "ads-log-daemon")
+logger = logging.getLogger(__name__)
 
 
 def to_logstash(
@@ -39,32 +51,88 @@ def to_logstash(
         "id": 0,  # hmm
         "event_class": "C0FFEEC0-FFEE-COFF-EECO-FFEEC0FFEEC0",
         "msg": message.message.decode("latin-1"),
-        "source": "logging.aggregator/Translator",
+        "source": "logging.aggregator/PythonLogDaemon",
         "event_type": 0,  # hmm
         "json": json.dumps(custom_json),
     }
 
 
-async def test():
-    handler = ads_async.log.configure(level="DEBUG")
+async def client_loop(
+    their_host: str,
+    their_net_id: Optional[str] = None,
+    our_net_id: Optional[str] = None,
+    add_log_filter: bool = True,
+    add_route: bool = True,
+):
+    our_net_id = our_net_id or LOG_DAEMON_NET_ID
+    their_net_id = their_net_id or f"{their_host}.1.1"
 
-    our_net_id = "172.21.148.164.1.1"
-    their_net_id = "172.21.148.227.1.1"
-    ip_and_port = ("localhost", 48898)
+    if add_log_filter:
+        # Filter the circuit-level messages:
+        handler.addFilter(ads_async.log.AddressFilter(our_net_id, their_net_id))
 
-    # View only the circuit-level messages:
-    handler.addFilter(ads_async.log.AddressFilter(our_net_id, their_net_id))
+    plc_info = next(get_plc_info(their_host))
+    logger.debug("Host: %s Got PLC info: %s", their_host, plc_info)
+    if plc_info["source_net_id"] != their_net_id:
+        logger.warning(
+            "PLC reports Net ID: %s Configured Net ID: %s; using PLC-reported Net ID.",
+            plc_info["source_net_id"],
+            their_net_id,
+        )
+        their_net_id = plc_info["source_net_id"]
 
-    async with Client(ip_and_port, our_net_id=our_net_id) as client:
+    if add_route:
+        logger.info("Adding route to PLC %s", their_host)
+        add_route_to_plc(
+            their_host,
+            source_net_id=our_net_id,
+            source_name=LOG_DAEMON_HOST,
+            route_name=LOG_DAEMON_HOST, # LOG_DAEMON_ROUTE_NAME
+        )
+        logger.info("Added route to PLC %s", their_host)
+
+    async with Client((their_host, constants.ADS_TCP_SERVER_PORT), our_net_id=our_net_id) as client:
         async with client.get_circuit(their_net_id) as circuit:
             device_info = await circuit.get_device_information()
-            circuit.log.info("Device info: %s", device_info)
-            project_name = await circuit.get_project_name()
-            circuit.log.info("Project name: %s", project_name)
-            app_name = await circuit.get_app_name()
-            circuit.log.info("Application name: %s", app_name)
-            task_names = await circuit.get_task_names()
-            circuit.log.info("Task names: %s", task_names)
+            try:
+                project_name = await circuit.get_project_name()
+            except RequestFailedError as ex:
+                # logger.warning("Failed to get project name from %s (%s)", their_host, ex)
+                project_name = ''
+
+            try:
+                app_name = await circuit.get_app_name()
+            except RequestFailedError as ex:
+                # logger.warning("Failed to get application name: %s (%s)", their_host, ex)
+                app_name = ''
+
+            try:
+                task_names = await circuit.get_task_names()
+            except RequestFailedError as ex:
+                # logger.warning("Failed to get task names: %s (%s)", their_host, ex)
+                task_names = []
+
+            logger.info("Service PLC info: %s", plc_info)
+            logger.info("PLC Device info: %s (%s)", device_info.version, device_info.name)
+            # Project name such as "Project1"
+            logger.info("Project name: %r", project_name)
+            # Application name such as "Port_851"
+            logger.info("Application name: %r", app_name)
+            # Task names such as ["MAIN_PlcTask", ...]
+            logger.info("Task names: %s", task_names)
+
+            plc_identifier = {
+                "net_id": plc_info["source_net_id"],
+                "address": their_host,
+                "name": plc_info["plc_name"],
+                "version": '{}.{}.{}'.format(*device_info.version.as_tuple),
+                "device_info_name": device_info.name,
+                "project_name": project_name,
+                "task_names": task_names,
+                # Can also get like `stLibVersion_Tc3_Module` or for LCLS general, etc.
+
+            }
+            logger.info("PLC identifier: %s", plc_identifier)
 
             # Give some time for initial notifications, and prune any stale
             # ones from previous sessions:
@@ -74,13 +142,16 @@ async def test():
                 try:
                     message = sample.as_log_message()
                 except Exception:
-                    circuit.log.exception("Got a bad log message sample? %s", sample)
+                    logger.exception("Got a bad log message sample? %s", sample)
                     continue
 
-                circuit.log.info(
+                logger.info(
                     "Log message %s ==> %s", message, to_logstash(header, message)
                 )
 
 
 if __name__ == "__main__":
-    value = asyncio.run(test(), debug=True)
+    logging.basicConfig(format=ads_async.log.PLAIN_LOG_FORMAT, level="INFO")
+    handler = ads_async.log.configure(level="DEBUG")
+    plc_host = sys.argv[1]
+    value = asyncio.run(client_loop(plc_host), debug=True)
